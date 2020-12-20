3d5c59eca10e413ad4c398fb12a7bd509e8f58b9
Ydot19 <abdalhalim.yaadata@gmail.com>
2020-12-19 20:10:37 -0800

    dev: updated file names and test

---
+++ tests/unit/test_list-to-map.py
@@ -0,0 +1,12 @@
+from src.utils import list_to_map as util
+
+
+def test_creat_map_from_str_list_no_special_character():
+    # Arrange
+    sample = ['676\t538\t4\t892685437', '721\t262\t3\t877137285', '913\t209\t2\t881367150']
+    # Act
+    actual = util.creat_map_from_str_list(sample, '\t', 0, 2)
+    # Assert
+    expect = {'676': '4', '721': '3', '913': '2'}
+    assert isinstance(actual, dict)
+    assert expect == actual
